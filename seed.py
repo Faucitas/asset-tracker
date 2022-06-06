6d9cfccc1efc3989730e7c7c660e6d631eb45d8e
Tommy Smith <tommy@tommyesmith.com>
2022-06-06 16:40:24 -0400

    remove old code fro seed file

--- seed.py
+++ seed.py
@@ -7,13 +7,11 @@ from api.user.models import User
 
 
 USER_COUNT = 50
-ACCOUNT_COUNT = 100
 ACCOUNT_TYPES = ['Crypto', 'Stocks', 'Bank', 'Credit']
 
 
 def truncate_tables():
     """Delete all rows from database tables"""
-    # Account.query.delete()
     User.query.delete()
     db.session.commit()
 
@@ -38,14 +36,6 @@ def main():
             'password': fake.password(length=256)
         }
         User.create(**rand_user)
-
-    # for _ in range(ACCOUNT_COUNT):
-    #     rand_account = {
-    #         'name': fake.company(),
-    #         'type': ACCOUNT_TYPES[rd.randint(0, len(ACCOUNT_TYPES) - 1)],
-    #         'user_id': get_random_user_id()
-    #     }
-    #     Account.create(**rand_account)
 
 
 if __name__ == '__main__':
