334e3a76843d7ca7b575fbaabae1fdc1a1dc913f
Tommy Smith <tommy@tommyesmith.com>
2022-06-09 17:08:41 -0400

    Add account routes

--- api/account/models/account.py
+++ api/account/models/account.py
@@ -1,8 +1,8 @@
-from api.database import Column, Model, db
+from api.database import Column, PKModel, db
 from api.extentions import ma
 
 
-class Account(Model):
+class Account(PKModel):
     __tablename__ = 'accounts'
     account_number = Column(db.String(128), nullable=False)
     strategy = Column(db.String(128), nullable=True)
@@ -17,6 +17,8 @@ class AccountSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Account
 
+    id = ma.auto_field()
     account_number = ma.auto_field()
     strategy = ma.auto_field(required=False)
     custodian = ma.auto_field()
+
